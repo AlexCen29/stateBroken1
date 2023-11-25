15aa120aa6ce088a7f13b519cc1a8b4f318595b4
addiel puc <22090642@alumno-utmetropolitana.edu.mx>
2023-11-25 11:28:52 -0600

    datos de clientes nuevos creador

--- src/components/listaClientesRecientes/ListaClientesRecientes.tsx
+++ src/components/listaClientesRecientes/ListaClientesRecientes.tsx
@@ -1,58 +1,95 @@
+import React, { useState, useEffect } from "react";
 import "../../../src/styles/index.css";
 import "./ListaClientesRecientes.css";
 
+interface Cliente {
+  id: number;
+  nombre: string;
+  fechaCreacion: string;
+  empleado: {
+    id: number;
+    nombre: string;
+    imagen: string;
+  };
+}
+
+const clientesPrueba: Cliente[] = Array.from({ length: 10 }, (_, i) => ({
+  id: i,
+  nombre: `Cliente ${i}`,
+  fechaCreacion: new Date().toISOString(),
+  empleado: {
+    id: i,
+    nombre: `Empleado ${i}`,
+    imagen: `url-imagen-${i}`,
+  },
+}));
+
 function ListaClientesRecientes() {
+  const [clientes, setClientes] = useState<Cliente[]>([]);
+  const [error, setError] = useState<string | null>(null);
 
-    return (
-        
-        <div className="container principal">
-            <h1>Clientes agregados recientemente</h1>
+  useEffect(() => {
+    const cookie = document.cookie
+      .split("; ")
+      .find((row) => row.startsWith("token"));
+    if (!cookie) {
+      throw new Error("Token not found");
+    }
 
-            <div className="header">
-                <div className="header-stretch"></div>
+    const token = cookie.split("=")[1];
+    fetch("http://jimenezmiapi.somee.com/api/Cliente/ultimos5", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Hubo un error al obtener los datos");
+        }
+        return response.json();
+      })
+      .then((data) => setClientes([...data, ...clientesPrueba])) // Añadir los datos de prueba después de los datos de la API // Solo se muestran los primeros 5 clientes
+      .catch((error) =>
+        setError(
+          "Un problema al cargar los datos. Por favor, inténtalo de nuevo más tarde."
+        )
+      );
+  }, []);
 
-                <div className="header-content">
-                    <div className="name">
-                        Reynaldo Avilés Coronado
-                    </div>
-
-                    <div className="initial">
-                        J
-                    </div>
-
-                    <div className="agent-name">
-                        Javier Rejón Gómez
-                    </div>
-
-                    <div className="agent-label">
-                        ~Agente
-                    </div>
-                </div>
-            </div>
-
+  return (
+    <>
+      <div className="container" id="clients">
+        <h1>Clientes agregados recientemente</h1>
+        <div className="principal">
+          {error && (
             <div className="item">
-                <div className="name">
-                    Laura Patricia González Ríos
-                </div>
-
-                <div className="initial">
-                    J
-                </div>
-
-                <div className="agent-name">
-                    Javier Rejón Gómez
-                </div>
-
-                <div className="agent-label">
-                    ~Agente
-                </div>
+              <div className="name">{error}</div>
             </div>
+          )}
 
+          {clientes.length === 0 ? (
             <div className="item">
+              <div className="name">No hay clientes recientes</div>
             </div>
+          ) : (
+            clientes.slice(0, 9).map((cliente, index) => (
+              <div className="item" key={`${cliente.id}-${index}`}>
+                <div className="name">{cliente.nombre}</div>
+                <div className="broker">
+                  <div className="picture">
+                    {cliente.empleado.nombre.charAt(0).toUpperCase()}
+                  </div>
+                  <div className="agent-name">{cliente.empleado.nombre}</div>
+                </div>
+                <div className="agent-label">~{cliente.empleado.imagen}</div>
+              </div>
+            ))
+          )}
         </div>
-    );
-
+      </div>
+    </>
+  );
 }
 
 export default ListaClientesRecientes;
